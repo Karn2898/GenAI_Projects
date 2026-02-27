aee80a6f6806520a30a11753c409c64883e8502a
Tamaghna Sarkar <tamaghna52@gmail.com>
2026-02-27 21:35:45 +0530

    updates code

--- Interview_question_generator/setup.py
+++ Interview_question_generator/setup.py
@@ -6,3 +6,15 @@ setup(name="Interview question Generator",
       packages=find_packages(),
       install_requires=[]
 )
+ 
+@app.post("/upload")
+async def chat(request: Request , pdf_file :bytes =File(), filename :str =Form(...)):
+    base_folder='static/docs/'
+    if not os.path.exists(base_folder):
+        os.makedirs(base_folder)
+    pdf_filename=os.path.join(base_folder,filename)
+    
+    async with aiofiles.open(pdf_filename, 'wb') as f:
+        await f.write(pdf_file)
+    response_data= jsonable_encoder(json.dumps({"msg": 'success' , "pdf_filename": pdf_filename}))  res=Response(response_data) return res
+    
