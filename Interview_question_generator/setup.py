cba9def31c54e789bb553cb27bd480a0e7bc6ffe
Tamaghna Sarkar <tamaghna52@gmail.com>
2026-02-27 21:08:54 +0530

    initial

---
+++ Interview_question_generator/setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages , setup
+setup(name="Interview question Generator",
+      version="0.0.0",
+      author="Tamaghna Sarkar",
+      author_email="tamaghna5@gmail.com",
+      packages=find_packages(),
+      install_requires=[]
+)
